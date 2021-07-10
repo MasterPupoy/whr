9a5dd9962240c902c4fbe64335f273caedea948d
masterpupoy <dellosonaryan@gmail.com>
2021-07-10 16:39:42 +0800

    build v1.3

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import Apply from './pages/Apply';
 function App() {
 
   return(
-    <Router basename='/'>
+    <Router>
       <Switch>  
         <Route exact path="/" component={Careers} />
         <Route exact path="/register" component={Register} />
