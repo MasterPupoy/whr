1f197b05c0f20797b2445ff61a3aabea9034f149
masterpupoy <dellosonaryan@gmail.com>
2021-07-12 19:18:41 +0800

    mongoURI changes

--- fileService/fileService.js
+++ fileService/fileService.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(fileUpload());
 
 
-mongoose.connect(`mongodb+srv://master_pupoy:wi7nB3Tv78go5DJQ@cluster1.zhjgs.mongodb.net/whr3?retryWrites=true&w=majority`, {
+mongoose.connect(`${DB_URI}`, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
